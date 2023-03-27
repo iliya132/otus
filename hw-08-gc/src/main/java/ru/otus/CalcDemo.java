f0b1f307bba841608528e64fa26ca491e2c5910b
iliya132 <iliya132@yandex.ru>
2023-03-27 22:11:59 +0300

    hw08-gc

--- hw-08-gc/src/main/java/ru/otus/CalcDemo.java
+++ hw-08-gc/src/main/java/ru/otus/CalcDemo.java
@@ -1,21 +1,10 @@
 package ru.otus;
 
-
-/*
--Xms256m
--Xmx256m
--XX:+HeapDumpOnOutOfMemoryError
--XX:HeapDumpPath=./logs/heapdump.hprof
--XX:+UseG1GC
--Xlog:gc=debug:file=./logs/gc-%p-%t.log:tags,uptime,time,level:filecount=5,filesize=10m
-*/
-
-
 import java.time.LocalDateTime;
 
 public class CalcDemo {
     public static void main(String[] args) {
-        long counter = 100_000_000;
+        var counter = 100_000_000;
         var summator = new Summator();
         long startTime = System.currentTimeMillis();
 
