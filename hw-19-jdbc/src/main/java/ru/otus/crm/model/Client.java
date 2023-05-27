fc0e3623ca5186a1460db76d73a57c6d8b0ad2f1
iliya132 <iliya132@yandex.ru>
2023-05-28 00:02:12 +0300

    finished hw-19-jdbc

---
+++ hw-19-jdbc/src/main/java/ru/otus/crm/model/Client.java
@@ -0,0 +1,49 @@
+package ru.otus.crm.model;
+
+import ru.otus.jdbc.mapper.Column;
+import ru.otus.jdbc.mapper.Id;
+
+public class Client {
+    @Id
+    @Column("id")
+    private Long id;
+    @Column("name")
+    private String name;
+
+    public Client() {
+    }
+
+    public Client(String name) {
+        this.id = null;
+        this.name = name;
+    }
+
+    public Client(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
